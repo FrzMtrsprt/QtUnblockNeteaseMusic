48215b68f703e38fb8bc5a140dbfeadebf1b2970
FrzMtrsprt <alex20020703@163.com>
2022-07-29 14:44:26 +0800

    Add icons

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/res/icon.ico"));
     QCoreApplication::setOrganizationName("FrzMtrsprt");
     QCoreApplication::setApplicationName("QtUnblockNeteaseMusic");
 
