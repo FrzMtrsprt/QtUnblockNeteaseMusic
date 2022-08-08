d82ccf89478ca1dd643f6cd6b4c8002bdf9f6047
FrzMtrsprt <alex20020703@163.com>
2022-08-08 11:16:46 +0800

    Bump version

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setWindowIcon(QIcon(":/res/icon.ico"));
     a.setApplicationName("QtUnblockNeteaseMusic");
-    a.setApplicationVersion("1.1.0");
+    a.setApplicationVersion("1.1.1");
     a.setOrganizationName("FrzMtrsprt");
     a.setOrganizationDomain("https://github.com/FrzMtrsprt/QtUnblockNeteaseMusic");
 
