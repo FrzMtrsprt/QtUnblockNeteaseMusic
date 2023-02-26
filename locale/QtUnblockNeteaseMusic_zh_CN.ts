389cd70879ffc9ae61735fb0b873071feb1071d1
FrzMtrsprt <alex20020703@163.com>
2023-02-27 00:26:01 +0800

    Remove locations from translation files

--- locale/QtUnblockNeteaseMusic_zh_CN.ts
+++ locale/QtUnblockNeteaseMusic_zh_CN.ts
@@ -4,12 +4,10 @@
 <context>
     <name>EnvDialog</name>
     <message>
-        <location filename="../src/envdialog.ui" line="9"/>
         <source>Edit Environment Variables</source>
         <translation>编辑环境变量</translation>
     </message>
     <message>
-        <location filename="../src/envdialog.ui" line="15"/>
         <source>VARIABLE_NAME_1=variable_value_1
 VARIABLE_NAME_2=variable_value_2</source>
         <translation>变量名称_1=变量值_1
@@ -19,194 +17,150 @@ VARIABLE_NAME_2=variable_value_2</source>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../src/mainwindow.ui" line="43"/>
         <source>Exit</source>
         <translation>退出</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="54"/>
         <source>Arguments</source>
         <translation>参数</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="99"/>
-        <location filename="../src/mainwindow.ui" line="177"/>
         <source>Set priority of sources</source>
         <translation>设置音源的优先级</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="164"/>
-        <location filename="../src/mainwindow.ui" line="194"/>
         <source>Force the Netease server IP</source>
         <translation>覆写网易服务器的 IP 地址</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="197"/>
         <source>Force Host</source>
         <translation>覆写地址</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="184"/>
-        <location filename="../src/mainwindow.ui" line="231"/>
         <source>Enable proxy limitation</source>
         <translation>启用代理限制</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="187"/>
         <source>Strict Mode</source>
         <translation>严格模式</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="89"/>
-        <location filename="../src/mainwindow.ui" line="204"/>
         <source>Specify server host</source>
         <translation>指定主机地址</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="207"/>
         <source>Address</source>
         <translation>地址</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="76"/>
-        <location filename="../src/mainwindow.ui" line="135"/>
         <source>Request through upstream proxy</source>
         <translation>使用代理发送请求</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="138"/>
         <source>Proxy URL</source>
         <translation>代理 URL</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="109"/>
         <source>Specify server port</source>
         <translation>指定服务器端口</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="112"/>
         <source>Port</source>
         <translation>端口</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="102"/>
         <source>Sources</source>
         <translation>音源</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="251"/>
         <source>Output</source>
         <translation>输出</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="338"/>
-        <location filename="../src/mainwindow.cpp" line="207"/>
         <source>About</source>
         <translation>关于</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="347"/>
         <source>About Qt</source>
         <translation>关于 Qt</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="15"/>
         <source>Run on startup</source>
         <translation>开机启动</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="22"/>
         <source>System Proxy</source>
         <translation>系统代理</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="36"/>
         <source>Apply</source>
         <translation>应用</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="60"/>
-        <location filename="../src/mainwindow.ui" line="214"/>
         <source>Enable HTTPS proxy</source>
         <translation>启用 HTTPS 代理</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="63"/>
         <source>HTTPS</source>
         <translation>HTTPS</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="125"/>
         <source>Specify server HTTP port</source>
         <translation>指定服务器 HTTP 端口</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="151"/>
         <source>Specify server HTTPS port</source>
         <translation>指定服务器 HTTPS 端口</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="270"/>
         <source>Show debug information</source>
         <translation>显示调试信息</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="284"/>
         <source>&amp;File</source>
         <translation>文件(&amp;F)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="290"/>
         <source>&amp;Help</source>
         <translation>帮助(&amp;H)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="297"/>
         <source>&amp;View</source>
         <translation>查看(&amp;V)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="301"/>
         <source>&amp;Theme</source>
         <translation>主题(&amp;T)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="312"/>
         <source>&amp;Advanced</source>
         <translation>高级(&amp;A)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="329"/>
         <source>E&amp;xit</source>
         <translation>退出(&amp;E)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="352"/>
         <source>&amp;Install Certificate</source>
         <translation>安装证书(&amp;I)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="357"/>
         <source>&amp;Environment Variables</source>
         <translation>环境变量(&amp;E)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="194"/>
         <source>&lt;h3&gt;About %1&lt;/h3&gt;&lt;p&gt;Version %2&lt;/p&gt;</source>
         <translation>&lt;h3&gt;关于 %1&lt;/h3&gt;&lt;p&gt;版本 %2&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="200"/>
         <source>&lt;p&gt;A desktop client for UnblockNeteaseMusic, made with Qt.&lt;/p&gt;&lt;p&gt;Copyright 2023 %1&lt;/p&gt;</source>
         <translation>&lt;p&gt;适用于 UnblockNeteaseMusic 的桌面应用，使用 Qt 制作。&lt;/p&gt;&lt;p&gt;版权所有 2023 %1&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="110"/>
         <source>Error</source>
         <translation>错误</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="112"/>
         <source>Failed to set system proxy.
 Please check the server port and address, and try again.</source>
         <translation>无法设置系统代理。
@@ -216,22 +170,18 @@ Please check the server port and address, and try again.</source>
 <context>
     <name>Server</name>
     <message>
-        <location filename="../src/server.cpp" line="65"/>
         <source>Node.js is not installed.</source>
         <translation>未安装 Node.js。</translation>
     </message>
     <message>
-        <location filename="../src/server.cpp" line="130"/>
         <source>Server not found.</source>
         <translation>未找到服务器。</translation>
     </message>
     <message>
-        <location filename="../src/server.cpp" line="148"/>
         <source>Server error</source>
         <translation>服务器错误</translation>
     </message>
     <message>
-        <location filename="../src/server.cpp" line="150"/>
         <source>The UnblockNeteaseMusic server ran into an error.
 Please change the arguments or check port usage and try again.</source>
         <translation>UnblockNeteaseMusic 服务器出现错误。
@@ -241,17 +191,14 @@ Please change the arguments or check port usage and try again.</source>
 <context>
     <name>Tray</name>
     <message>
-        <location filename="../src/tray.cpp" line="14"/>
         <source>Show</source>
         <translation>显示</translation>
     </message>
     <message>
-        <location filename="../src/tray.cpp" line="15"/>
         <source>System Proxy</source>
         <translation>系统代理</translation>
     </message>
     <message>
-        <location filename="../src/tray.cpp" line="17"/>
         <source>Exit</source>
         <translation>退出</translation>
     </message>
